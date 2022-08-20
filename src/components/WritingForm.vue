b546c3b7d0d2eac6e6ce962377b008eb05c5ad58
Pinixk <bjkim0110@gmail.com>
2022-08-21 00:51:53 +0900

    08/21

--- src/components/WritingForm.vue
+++ src/components/WritingForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="quill">
-    <quill-editor theme="snow"></quill-editor>
+    <quill-editor :options="options"></quill-editor>
   </div>
 </template>
 
@@ -9,12 +9,35 @@ import { QuillEditor } from "@vueup/vue-quill";
 import "@vueup/vue-quill/dist/vue-quill.snow.css";
 
 export default {
-  components: { QuillEditor },
-}
+  components: {
+    QuillEditor,
+  },
+  data() {
+    return {
+      options: {
+        debug: "info",
+        modules: {
+          syntax: true,
+          toolbar: [
+            [{ font: [] }, { size: [] }],
+            ["bold", "italic", "underline"],
+            [{ color: [] }, { background: [] }],
+            ["blockquote"],
+            [{ list: "ordered" },{ list: "bullet" },],
+            ["image", "video"],
+            ["clean"],
+          ],
+        },
+        placeholder: "Compose an epic...",
+        theme: "snow",
+      },
+    };
+  },
+};
 </script>
 
 <style>
-.quill{
+.quill {
   width: 60vw;
   height: 35rem;
   margin: 10px auto;
--- src/views/BoardWrite.vue
+++ src/views/BoardWrite.vue
@@ -1,26 +1,41 @@
 <template>
-  <h1 class="title">{{message}}</h1>
-  <div>
-    <h3>제목 : </h3>
-    <WritingForm/>
-  </div>
+  <h1 class="title">{{ message }}</h1>
+  <div class="subject">
+    <h3>제목 :</h3>
+    <input class="form-control form-control sub" type="text" />
+  </div><br>
+  <WritingForm />
+  <button type="button" class="btn btn-danger recode">Cancel</button>
+  <button type="button" class="btn btn-danger recode">Regist</button>
 </template>
 
 <script>
-import WritingForm from '../components/WritingForm.vue'
+import WritingForm from "../components/WritingForm.vue";
 
 export default {
-  name: 'BoardWrite',
-  components: {WritingForm},
-  data(){
-    return {message:"Writing"}
-  }
+  name: "BoardWrite",
+  components: { WritingForm },
+  data() {
+    return { message: "Writing" };
+  },
 };
 </script>
 
 <style scoped>
-h2{
+.subject {
+  display: flex;
+  margin-left: 250px;
+}
+h3 {
   display: inline-block;
-  margin: 0px;
+}
+.sub {
+  display: inline-block;
+  width: 20%;
+  margin-left: 20px;
+}
+.btn {
+  position: relative;
+  left: 100px;
 }
 </style>
