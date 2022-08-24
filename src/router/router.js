accd9593f43993e347d4a0d0a4c5f91467268bf3
Pinixk <bjkim0110@gmail.com>
2022-08-24 17:04:29 +0900

    08/24

--- src/router/router.js
+++ src/router/router.js
@@ -2,10 +2,10 @@ import { createWebHistory, createRouter } from "vue-router";
 import Main from "@/views/Main.vue";
 import Login from "@/views/Login.vue";
 import Join from "@/views/Join.vue";
-import WoCreate from "@/views/WoCreate.vue";
-import WoRecord from "@/views/WoRecord.vue";
-import WoList from "@/views/WoList.vue";
-import WoDetail from "@/views/WoDetail.vue";
+import WorkoutCreate from "@/views/WorkoutCreate.vue";
+import WorkoutRecord from "@/views/WorkoutRecord.vue";
+import WorkoutList from "@/views/WorkoutList.vue";
+import WorkoutDetail from "@/views/WorkoutDetail.vue";
 import Board from "@/views/Board.vue";
 import BoardWrite from "@/views/BoardWrite.vue";
 import BoardContent from "@/views/BoardContent.vue";
@@ -17,10 +17,10 @@ const routes = [
   {path: "/", name: "main", component: Main},
   {path: "/login", name: "login", component: Login},
   {path: "/join", name: "join", component: Join},
-  {path: "/workout/create", name: "woCreate", component: WoCreate},
-  {path: "/workout/record", name: "woRecord", component: WoRecord},
-  {path: "/workout/list", name: "woList", component: WoList},
-  {path: "/workout/detail", name: "woDetail", component: WoDetail},
+  {path: "/workout/create", name: "woCreate", component: WorkoutCreate},
+  {path: "/workout/record", name: "woRecord", component: WorkoutRecord},
+  {path: "/workout/list", name: "woList", component: WorkoutList},
+  {path: "/workout/detail", name: "woDetail", component: WorkoutDetail},
   {path: "/board", name: "board", component: Board},
   {path: "/board/write", name: "boardWrite", component: BoardWrite},
   {path: "/board/content", name: "boardContent", component: BoardContent},
