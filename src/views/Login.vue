b7b9f1aa5b29060f935372b3b763e8ac0738040e
ktj0 <xoet418@naver.com>
2022-08-24 16:00:35 +0900

    변경

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,38 @@
 <template>
-  <h1>{{message}}</h1>
-  <h1>Test</h1>
+  <div class="all">
+  <form action="">
+  <div class="login-box">
+
+  <h3>로그인</h3>
+  <hr>
+
+  <div class="row mb-3">
+  <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">ID</label>
+  <div class="col-sm-10">
+  <input type="text" class="form-control form-control-sm" id="colFormLabelSm" placeholder="내용을 입력해주세요">
+  </div>
+  </div>
+
+  <div class="row mb-3">
+  <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Password</label>
+  <div class="col-sm-10">
+  <input type="password" class="form-control form-control-sm" id="colFormLabelSm" placeholder="내용을 입력해주세요">
+  </div>
+  </div>
+
+  <div class="button-group">
+  <div class="log-button">
+  <button type="button" class="btn btn-danger btn-sm">로그인</button>
+  </div>
+
+  <div class="join-button">
+  <button type="button" class="btn btn-danger btn-sm">회원가입</button>
+  </div>
+  </div>
+  
+  </div>
+  </form>
+ </div>
 </template>
 
 <script>
@@ -13,5 +45,26 @@ export default {
 </script>
 
 <style>
-
+.all{
+  text-align: center
+}
+.login-box{
+  height: 300px;
+	width: 400px;
+  display: inline-block;
+  text-align: left;
+}
+h2{
+  text-align: left;
+}
+.button-group{
+  text-align: right;
+}
+.log-button{
+  display: inline-block;
+  padding-right: 20px;
+}
+.join-button{
+  display: inline-block;
+}
 </style>
