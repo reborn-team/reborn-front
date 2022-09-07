cadf2678d4f1816dc0c2529bb762095e60638b61
Pinixk <bjkim0110@gmail.com>
2022-09-07 17:25:59 +0900

    ADD : 비밀번호 변경 모달

--- src/views/MyPageChange.vue
+++ src/views/MyPageChange.vue
@@ -22,7 +22,14 @@
       <!-- 비밀번호 -->
       <div class="row mb-3">
         <label for="user_password" class="form-label">Password</label>
-        <button type="button" class="btn btn-danger btn-sm">비밀번호 변경</button>
+        <button
+          type="button"
+          class="btn btn-danger btn-sm"
+          data-bs-toggle="modal"
+          data-bs-target="#changePassword"
+        >
+          비밀번호 변경
+        </button>
       </div>
 
       <!-- 이름 -->
@@ -94,7 +101,6 @@
         <VueDaumPostcode @complete="oncomplete" />
       </div>
 
-      
       <div id="changeBtn">
         <div class="change-button">
           <button class="btn btn-warning btn-sm delete" @click="deletehandler">
@@ -110,6 +116,53 @@
           <button class="btn btn-danger btn-sm" @click="changeHandler">
             완료
           </button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- 모달 -->
+  <div
+    class="modal fade"
+    id="changePassword"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body" id="modalBody">
+          <div>
+            <label for="" class="form-label">현재 비밀번호</label>
+            <input type="text" class="form-control form-control-sm" ref="" />
+          </div>
+          <div>
+            <label for="" class="form-label">새 비밀번호</label>
+            <input type="text" class="form-control form-control-sm" ref="" />
+          </div>
+          <div>
+            <label for="" class="form-label">새 비밀번호 확인</label>
+            <input type="text" class="form-control form-control-sm" ref="" />
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            취소
+          </button>
+          <button type="button" class="btn btn-danger">변경하기</button>
         </div>
       </div>
     </div>
@@ -218,5 +271,3 @@ export default {
   },
 };
 </script>
-
-
