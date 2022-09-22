16dd54207fc84e6c5a3f4cd5f6f92ec2cadbc49e
pinixk <bjkim0110@gmail.com>
2022-09-22 16:28:44 +0900

    Add: 운동일지 생성 API 완료

--- src/views/BoardWrite.vue
+++ src/views/BoardWrite.vue
@@ -3,7 +3,7 @@
     <h1 class="title">{{ message }}</h1>
     <div id="subject">
       <h3>제목 :</h3>
-      <input class="form-control form-control sub" type="text" />
+      <input class="form-control form-control sub" type="text" ref="title" v-model="state.title"/>
     </div>
     <div class="content">
       <textarea
@@ -11,6 +11,8 @@
         aria-label="With textarea"
         rows="15"
         maxlength="300"
+        ref="content"
+        v-model="state.content"
       ></textarea>
     </div>
     <div class="fileUpload">
@@ -34,16 +36,74 @@
 <script>
 import router from "@/router/router";
 import "../css/views/BoardWrite.css";
+import { reactive, ref } from '@vue/reactivity';
+import axios from 'axios';
 
 export default {
   name: "BoardWrite",
   setup() {
-    const registArticle = () => {
-      alert("등록 되었습니다");
+
+    const state = reactive({
+      title: "",
+      content: "",
+      token: sessionStorage.getItem("TOKEN"),
+    })
+    const title = ref("");
+    const content = ref("");
+    let files = ref([]);
+    let originFileName = ref("");
+    let uploadFileName = ref("");
+
+    const registArticle = async() => {
+      const url = "/api/v1/articles ";
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: state.token,
+      };
+      const body = {
+        title: state.title,
+        content: state.content,
+        originFileName: originFileName,
+        uploadFileName: uploadFileName,
+      };
+      await axios.post(url, body, {headers}).then((res)=>{
+        console.log(originFileName)
+        console.log(res.data);
+      })
+
       router.push("/board");
     };
 
-    return { registArticle, message: "글쓰기" };
+    const selectFile = (event) => {
+      const formData = new FormData();
+      for (const file of event.target.files) {
+        formData.append("file", file);
+      }
+      axios
+        .post("/api/v1/file", formData, {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((res) => {
+          if (res.status == 200) {
+            files.value = res.data;
+            originFileName = files.value[0].originFileName;
+            uploadFileName = files.value[0].uploadFileName;
+            console.log(uploadFileName)
+          }
+        })
+        .catch(() => {
+          alert("파일을 업로드 할 수 없습니다");
+        });
+    };
+
+    return { 
+      message: "글쓰기" ,
+      state,
+      title,
+      content,
+      registArticle,
+      selectFile,
+    };
   },
 };
 </script>
