7469589174557e82f3d39ef0b5f4db84cdc421d6
pinixk <bjkim0110@gmail.com>
2022-10-06 17:40:41 +0900

    Chore: 조회수 삭제

--- src/components/Board/List.vue
+++ src/components/Board/List.vue
@@ -7,7 +7,6 @@
           <th scope="col">제목</th>
           <th scope="col">작성자</th>
           <th scope="col">날짜</th>
-          <th scope="col">조회 수</th>
         </tr>
       </thead>
       <tbody class="table-group-divider" >
@@ -16,7 +15,6 @@
           <td>{{i.title}}</td>
           <td>{{i.memberNickname}}</td>
           <td>{{i.regDate}}</td>
-          <td>{{i.viewCount}}</td>
         </tr>
       </tbody>
     </table> 
