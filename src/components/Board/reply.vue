7aee4f151dc21bff4315a1b03aa3fa44bfe3e680
pinixk <bjkim0110@gmail.com>
2022-10-07 12:54:08 +0900

    Fix: 댓글 모달 제목 변경

--- src/components/Board/reply.vue
+++ src/components/Board/reply.vue
@@ -23,7 +23,7 @@
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+            <h5 class="modal-title" id="staticBackdropLabel">댓글 수정</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
